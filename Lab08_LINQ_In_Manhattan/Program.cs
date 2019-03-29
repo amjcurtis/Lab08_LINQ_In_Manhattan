5ff4057f799e81241c603de6ad0ce477eb94931b
Andrew Curtis <curtis.andrewm@gmail.com>
2019-03-29 11:49:04 -0700

    Add classes for JSON

--- Lab08_LINQ_In_Manhattan/Program.cs
+++ Lab08_LINQ_In_Manhattan/Program.cs
@@ -6,7 +6,16 @@ namespace Lab08_LINQ_In_Manhattan
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            //TODO Use StreamReader to read in JSON
+            // string path = "../data.json";
+
+            //TODO Deserialize JSON to object
+
+            //TODO Get neighborhoods and neighborhood names from deserialized JSON
+            //TODO Create classes for Neighborhood (and Name)?
+            
+
         }
     }
 }
