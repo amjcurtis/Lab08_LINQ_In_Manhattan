a416aa88d3cf3d2d00126fc6d7fb0efd9433c916
Andrew Curtis <curtis.andrewm@gmail.com>
2019-03-31 23:47:22 -0700

    Add comments for planning

--- Lab08_LINQ_In_Manhattan/Program.cs
+++ Lab08_LINQ_In_Manhattan/Program.cs
@@ -7,7 +7,9 @@ namespace Lab08_LINQ_In_Manhattan
         static void Main(string[] args)
         {
             
-            //TODO Use StreamReader to read in JSON
+            //TODO Use StreamReader to read in JSON and store in a variable
+                // Use StreamReader command "Read all lines" so it keeps formatting
+                // Then query the data under that variable name
             // string path = "../data.json";
 
             //TODO Deserialize JSON to object
