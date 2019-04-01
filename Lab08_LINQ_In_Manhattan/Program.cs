462a0e1066233e92b27e1a252173af15d5161d87
Andrew Curtis <curtis.andrewm@gmail.com>
2019-04-01 10:20:22 -0700

    Add NewtonsoftJson dependency, set up file path, and deserialize JSON

--- Lab08_LINQ_In_Manhattan/Program.cs
+++ Lab08_LINQ_In_Manhattan/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using Lab08_LINQ_In_Manhattan.Classes;
+using Newtonsoft.Json;
 
 namespace Lab08_LINQ_In_Manhattan
 {
@@ -6,18 +9,29 @@ namespace Lab08_LINQ_In_Manhattan
     {
         static void Main(string[] args)
         {
-            
+
             //TODO Use StreamReader to read in JSON and store in a variable
-                // Use StreamReader command "Read all lines" so it keeps formatting
-                // Then query the data under that variable name
-            // string path = "../data.json";
+            // Use StreamReader command "Read all lines" so it keeps formatting
+            // Then query the data under that variable name
+            string path = "../../../../data.json";
+
+            var data = "";
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                data = sr.ReadToEnd();
+            }
 
             //TODO Deserialize JSON to object
+            RootObject json = JsonConvert.DeserializeObject<RootObject>(data);
+
+
 
             //TODO Get neighborhoods and neighborhood names from deserialized JSON
             //TODO Create classes for Neighborhood (and Name)?
-            
 
+
+            Console.ReadLine();
         }
     }
 }
